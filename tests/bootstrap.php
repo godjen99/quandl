f77c9bfec6a6ee2118fe9c6124176826977ed92c
Terry Rowland <godjen99@yahoo.com>
2016-05-22 16:37:10 -0400

    Wanted to add some comments to be clear on what dd is and why we are requiring the autoload file.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,12 @@
 <?php
+// Load all the composer dependencies.
 require __DIR__.'/../vendor/autoload.php';
 
+/**
+ * Dumps a given variable and terminates the program.
+ *
+ * @param $var
+ */
 function dd($var)
 {
     dump($var);
